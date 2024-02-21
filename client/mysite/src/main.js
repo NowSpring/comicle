dc780754f991aa58b5daa9bd3dc4e93bcd83bcda
NowSpring <kodai218@icloud.com>
2024-02-22 03:03:18 +0900

    add login function

--- client/mysite/src/main.js
+++ client/mysite/src/main.js
@@ -4,7 +4,17 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
+import Antd from "ant-design-vue";
+
+import "ant-design-vue/dist/antd.css";
+
 
 loadFonts();
 
-createApp(App).use(router).use(store).use(vuetify).mount("#app");
+const app = createApp(App);
+
+app.use(Antd);
+app.use(store);
+app.use(router);
+app.use(vuetify);
+app.mount("#app");
