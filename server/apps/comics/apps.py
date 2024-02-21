6aecc8a2c8e912d4cce6b1e28166083f032bfa5d
NowSpring <kodai218@icloud.com>
2024-02-22 00:23:49 +0900

    add the method deleting media files automatically

--- server/apps/comics/apps.py
+++ server/apps/comics/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ComicsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'comics'
+
+    def ready(self):
+      
+        import comics.signals
