6269a89e0cbc6e1bf5d78b2282142ec7c0a9fd9c
NowSpring <kodai218@icloud.com>
2024-02-23 03:22:56 +0900

    add codes to logout

--- client/mysite/src/views/HomeView.vue
+++ client/mysite/src/views/HomeView.vue
@@ -1,18 +1,39 @@
 <template>
   <HelloWorld />
+  <a-button type="primary mx-4" @click.prevent="logOut">ログアウト</a-button>
 </template>
 
-<script>
-import { defineComponent } from "vue";
 
-// Components
+<script setup>
+import { ref, onMounted } from 'vue';
+import router from '@/router';
+import EventService from "@/plugins/EventService.js";
+// import TicketTable from "@/components/TicketTable.vue";
+// import CreateModal from "@/components/CreateModal.vue";
 import HelloWorld from "../components/HelloWorld.vue";
 
-export default defineComponent({
-  name: "HomeView",
 
-  components: {
-    HelloWorld,
-  },
-});
+// const getTicketList = () => {
+//   EventService.getTickets()
+//     .then((response) => {
+//       tickets.value = response.data;
+//     })
+//     .catch((error) => {
+//       console.log("Error_home" + error);
+//     });
+// };
+
+// onMounted(() => {
+//   const token = localStorage.getItem('token');
+//   if (token !== null) {
+//     getTicketList(token);
+//   }
+// });
+
+const logOut = () => {
+  window.localStorage.removeItem('token');
+  window.localStorage.removeItem('user_id');
+  window.localStorage.removeItem('user_name');
+  router.push({ name: 'login' });
+};
 </script>
