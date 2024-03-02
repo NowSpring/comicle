8d49550ff5692433acd66605f45e10d3ca225f16
NowSpring <kodai218@icloud.com>
2024-03-03 03:21:00 +0900

    fix some models

--- server/apps/members/serializers.py
+++
@@ -1,9 +0,0 @@
-from drf_writable_nested import WritableNestedModelSerializer
-from members.models import Member
-
-class MemberSerializer(WritableNestedModelSerializer):
-  
-    class Meta:
-      
-        model = Member
-        fields = ["id", "user_name"] 
