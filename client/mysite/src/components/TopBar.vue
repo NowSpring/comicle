c89e84f79c7cb3014c58906e880c90c67522c3f8
NowSpring <kodai218@icloud.com>
2024-03-03 03:37:25 +0900

    Revert "fix some models"
    This reverts commit 8d49550ff5692433acd66605f45e10d3ca225f16.

---
+++ client/mysite/src/components/TopBar.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-app-bar
+    app
+    clippedLeft
+    :height="60"
+    color="primary"
+  >
+
+    <v-btn
+      icon
+      :to="{ name: 'comicmaster' }"
+    >
+      <v-icon x-large>
+        mdi-book-open-variant
+      </v-icon>
+    </v-btn>
+
+    <v-spacer></v-spacer>
+
+    <v-btn
+      text
+      :to="{ name: 'comicmaster' }" 
+    >
+      <span>{{ user_name }}</span>
+      <v-icon>mdi-account</v-icon>
+    </v-btn>
+
+    <v-btn
+      text
+      @click="logOut"
+    >
+      <span class="mr-2">
+        Logout
+      </span>
+      <v-icon>
+        mdi-open-in-new
+      </v-icon>
+    </v-btn>
+
+  </v-app-bar>
+  
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import router from '@/router';
+
+const user_name = ref('');
+
+onMounted(() => {
+  user_name.value = window.localStorage.getItem("user_name");
+}) 
+
+const logOut = () => {
+  window.localStorage.removeItem('token');
+  window.localStorage.removeItem('user_id');
+  window.localStorage.removeItem('user_name');
+  router.push({ name: 'login' });
+};
+</script>
---
+++ client/mysite/src/plugins/EventService.js
@@ -0,0 +1,58 @@
+import axios from "axios";
+
+// ログイン用のaxiosインスタンス
+const loginClient = axios.create({
+  baseURL: "http://0.0.0.0:8000/",
+  withCredentials: false,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+// API用のaxiosインスタンス
+const apiClient = axios.create({
+  baseURL: "http://0.0.0.0:8000/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// リクエストを送信する前に実行されるインターセプターを追加
+apiClient.interceptors.request.use((config) => {
+  const token = window.localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Token ${token}`; // トークンを動的に設定
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+
+export default {
+  submitLogin(logininfo) {
+    return loginClient.post("api-token-auth/", logininfo);
+  },
+  getComicMaster() {
+    return apiClient.get("comic/master");
+  },
+  getComicVersion() {
+    return apiClient.get("comic/version");
+  },
+  getComicEpisode() {
+    return apiClient.get("comic/episode");
+  },
+  // getReviewMaster() {
+  //   return apiClient.get("review/master");
+  // },
+  // postTicket(data) {
+  //   return apiClient.post("tickets/", data);
+  // },
+  // updateTicket(id, data) {
+  //   return apiClient.patch(`tickets/${id}/`, data);
+  // },
+  // deleteTicket(id) {
+  //   return apiClient.delete(`tickets/${id}`);
+  // },
+};
